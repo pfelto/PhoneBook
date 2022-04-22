a988ce898cb9eddcb7c878cdc9a572c148cbe887
pfelto <paul.r.felton@gmail.com>
2022-04-22 12:21:06 -0400

    Added React router and basic design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <nav style={{ border: "solid 1px" }}>
+        <Link to="/">Search Phone Book</Link>{" "}
+        <Link to="/list">Phone Book List</Link>{" "}
+        <Link to="/create">Create Contact</Link>
+      </nav>
+      <h1>Phone Book Search</h1>
+      <form>
+        <input type="text"></input>
+        <input type="submit"></input>
+      </form>
     </div>
   );
 }
